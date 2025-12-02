ec3be4f700bbf48bf8760e6f6487fbc1294f1e97
Clapiclapivara <clara.luizamsilva@gmail.com>
2025-12-02 12:25:43 -0300

    tarefa mod9

---
+++ tarefa_wrapper/Mod9/src/Teste.java
@@ -0,0 +1,15 @@
+public class Teste {
+
+
+    public static void main(String[] args) {
+
+        int codigo1 = 2172;
+       Integer codigo2 = codigo1;
+
+       System.out.println("wrapper: " + codigo1);
+
+
+
+    }
+
+}
